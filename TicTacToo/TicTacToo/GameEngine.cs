6a9cf06fc03aa0f5a881d4683072ff37e57419e5
Chester Hartin <chesterhartin@gmail.com>
2019-05-14 07:47:28 -0500

    initial check-in

---
+++ TicTacToo/TicTacToo/GameEngine.cs
@@ -0,0 +1,111 @@
+﻿using Xamarin.Forms;
+
+namespace TicTacToo
+{
+    /// <summary>
+    /// This is the game engine of Tic Tac Too
+    /// </summary>
+    public class GameEngine
+    {
+        /// <summary>
+        /// Which players turn is it
+        /// </summary>
+        private int _playerTurn = 1;
+
+        /// <summary>
+        /// Winning combinations
+        /// </summary>
+        private readonly int[,] _winningCombinations = new int[,]
+        {
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+            {0,3,6},
+            {1,4,7},
+            {2,5,8},
+            {0,4,8},
+            {2,4,6}
+
+        };
+
+        /// <summary>
+        /// Counter variable
+        /// </summary>
+        private int _counter;
+
+        /// <summary>
+        /// Check to see if a winner exists
+        /// </summary>
+        /// <returns><c>true</c>, if winner was checked, <c>false</c> otherwise.</returns>
+        /// <param name="buttons">Buttons.</param>
+        public bool CheckWinner(Button[] buttons)
+        {
+            bool gameOver = false;
+            for (_counter = 0; _counter < 8; _counter++)
+            {
+                int a = _winningCombinations[_counter, 0], b = _winningCombinations[_counter, 1], c = _winningCombinations[_counter, 2];
+                Button b1 = buttons[a], b2 = buttons[b], b3 = buttons[c];
+
+
+                if (b1.Text == "" || b2.Text == "" || b3.Text == "")
+                    continue;
+
+                if (b1.Text == b2.Text && b2.Text == b3.Text)
+                {
+                    b1.BackgroundColor = b2.BackgroundColor = b3.BackgroundColor = Color.Aqua;
+                    gameOver = true;
+                    break;
+                }
+            }
+
+            bool isTie = true;
+            if (!gameOver)
+
+            {
+                foreach (Button b in buttons)
+                {
+                    if (b.Text == "")
+                    {
+                        isTie = false;
+                        break;
+                    }
+                }
+                if (isTie)
+                {
+                    gameOver = true;
+                }
+            }
+            return gameOver;
+        }
+
+        /// <summary>
+        /// Change teh button
+        /// </summary>
+        /// <param name="b">The blue component.</param>
+        public void SetButton(Button b)
+        {
+            if (b.Text == "")
+            {
+                // depending on the current player, set the X or O
+                b.Text = _playerTurn == 1 ? "X" : "O";
+
+                // next players turn
+                _playerTurn = _playerTurn == 1 ? 2 : 1;
+            }
+        }
+
+        /// <summary>
+        /// Resets the game.
+        /// </summary>
+        /// <param name="buttons">Buttons.</param>
+        public void ResetGame(Button[] buttons)
+        {
+            _playerTurn = 1;
+            foreach (Button b in buttons)
+            {
+                b.Text = "";
+                b.BackgroundColor = Color.Silver;
+            }
+        }
+    }
+}
---
+++ TicTacToo/TicTacToo/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace TicTacToo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        /// <summary>
+        /// Set up a list of buttons to react to
+        /// </summary>
+        private Button[] _buttons = new Button[9];
+
+        /// <summary>
+        /// Create an instance of the game engine
+        /// </summary>
+        private GameEngine _game = new GameEngine();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:TicTacToo.MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _buttons[0] = button1;
+            _buttons[1] = button2;
+            _buttons[2] = button3;
+            _buttons[3] = button4;
+            _buttons[4] = button5;
+            _buttons[5] = button6;
+            _buttons[6] = button7;
+            _buttons[7] = button8;
+            _buttons[8] = button9;
+
+
+        }
+
+        /// <summary>
+        /// Handle the click event on a button
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void button_Clicked(object sender, EventArgs e)
+        {
+            _game.SetButton((Button)sender);
+
+            // check to see if there is a winner
+            if (_game.CheckWinner(_buttons))
+            {
+                // if so, show the game over stack
+                gameOverStackLayout.IsVisible = true;
+            }
+        }
+
+        /// <summary>
+        /// Play again 
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void playagain_Clicked(Object sender, EventArgs e)
+        {
+            // reset the game with the array of buttons
+            _game.ResetGame(_buttons);
+            // hide the game over stack
+            gameOverStackLayout.IsVisible = false;
+        }
+    }
+}
